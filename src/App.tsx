ced21eaf62004b6b87bb170291d05b5935a83464
FakeLabCoat <pmievo@gmail.com>
2026-01-27 14:05:40 -0500

    button fix

--- src/App.tsx
+++ src/App.tsx
@@ -155,7 +155,7 @@ function App() {
   const handleCalculate = (event?: React.SyntheticEvent) => {
     event?.preventDefault()
     event?.stopPropagation()
-    console.log('CALCULATE_CLICKED')
+    console.log("CALCULATE_CLICKED")
     setIsStale(false)
     const nextErrors: Errors = {}
     const maxLossValue = Number(maxLoss)
@@ -270,7 +270,7 @@ function App() {
     setResults(nextResults)
     setIsStale(false)
     setHasCalculated(true)
-    console.log('CALCULATE_DONE', {
+    console.log("CALCULATE_DONE", {
       calculatedFlagValue: true,
       outputsPreview: nextResults,
     })
